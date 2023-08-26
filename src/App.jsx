d5045ff469d24853eba74aafe1b1e19cc7a38764
AleBarboni <alexa_wera@yahoo.com>
2023-08-26 14:13:06 +0300

    Created NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
 //import * as React from 'react';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Box from '@mui/material/Box';
-import PersonIcon from '@mui/icons-material/Person';
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
-    <Box
-    sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'end',
-      '& > *': {
-        m: 1,
-      },
-    }}
-  >
-    <ButtonGroup variant="text" aria-label="text button group">
-      <Button startIcon={<PersonIcon />}>My Account</Button>
-      <Button>Favorites</Button>
-      <Button>Cart</Button>
-      <Button>Sign In</Button>
-    </ButtonGroup>
-  </Box>
+    <NavBar />
 );
 }
 
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,26 @@
+import {Box, ButtonGroup, Button} from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import LoginIcon from '@mui/icons-material/Login';
+
+function NavBar() {
+  return (
+    <Box
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'end',
+    }}
+  >
+    <ButtonGroup variant="text" aria-label="text button group">
+      <Button startIcon={<PersonIcon />}>My Account</Button>
+      <Button startIcon={<FavoriteIcon />}>Favorites</Button>
+      <Button startIcon={<ShoppingCartIcon />}>Cart</Button>
+      <Button startIcon={<LoginIcon />}>Log In</Button>
+    </ButtonGroup>
+  </Box>
+);
+}
+
+export default NavBar
